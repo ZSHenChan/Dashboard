982b75f2d52e8150a197b5136d73328de84e996f
ZSHenChan <zishenchan@gmail.com>
2026-02-24 12:53:31 +0800

    refac(lib): update gitignore to include lib/

---
+++ packages/dashboard-server/lib/s3_client.py
@@ -0,0 +1,8 @@
+import boto3
+from core.config import config
+
+s3_client = boto3.client(
+    "s3",
+    aws_access_key_id=config.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY
+)
